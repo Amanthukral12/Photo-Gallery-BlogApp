a4af016646698faf6e1d769e66462c9d7be7cbc6
Aman Thukral <thukral618@gmail.com>
2020-08-06 20:42:00 +0530

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index")
 
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/photogallery";
+var url = process.env.DATABASE_URL || "mongodb://localhost:27017/photogallery";
 
 mongoose
 	.connect(url, {
